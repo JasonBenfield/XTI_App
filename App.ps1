05ed0a8bd7a6088ae93b77e63267b2c0a2b25e7d
Jason Benfield <jbenfield108@gmail.com>
2020-10-18 07:10:36 -0400

    created solution

---
+++ App.ps1
@@ -0,0 +1,62 @@
+Import-Module PowershellForXti -Force
+
+$script:appConfig = [PSCustomObject]@{
+    RepoOwner = "JasonBenfield"
+    RepoName = "XTI_App"
+    AppKey = "XTI_App"
+    AppType = "Package"
+    ProjectDir = "C:\XTI\src\XTI_App\Apps\XTI_App"
+}
+
+function App-New-XtiIssue {
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string] $IssueTitle,
+        $Labels = @(),
+        [string] $Body = "",
+        [switch] $Start
+    )
+    $script:appConfig | New-XtiIssue @PsBoundParameters
+}
+
+function App-Xti-StartIssue {
+    param(
+        [Parameter(Position=0)]
+        [long]$IssueNumber = 0,
+        $IssueBranchTitle = "",
+        $AssignTo = ""
+    )
+    $script:appConfig | Xti-StartIssue @PsBoundParameters
+}
+
+function App-New-XtiVersion {
+    param(
+        [Parameter(Position=0)]
+        [ValidateSet("major", "minor", "patch")]
+        $VersionType = "minor",
+        [ValidateSet("Development", "Production", "Staging", "Test")]
+        $EnvName = "Production"
+    )
+    $script:appConfig | New-XtiVersion @PsBoundParameters
+}
+
+function App-New-XtiPullRequest {
+    param(
+        [Parameter(Position=0)]
+        [string] $CommitMessage
+    )
+    $script:appConfig | New-XtiPullRequest @PsBoundParameters
+}
+
+function App-Xti-PostMerge {
+    param(
+    )
+    $script:appConfig | Xti-PostMerge @PsBoundParameters
+}
+
+function App-Publish {
+    param(
+        [switch] $Prod
+    )
+    $script:appConfig | Xti-PublishPackage @PsBoundParameters
+}
