07e943825063ed36f6b129aa0577a46fdbf7950d
jbenfield108@gmail.com <jbenfield108@gmail.com>
2021-12-10 08:03:06 -0500

    Version V1354

---
+++ Lib/XTI_App.Hosting/AppAgendaHostedService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace XTI_App.Hosting;
+
+public sealed class AppAgendaHostedService : IHostedService
+{
+    private readonly AppAgenda agenda;
+
+    public AppAgendaHostedService(AppAgenda agenda)
+    {
+        this.agenda = agenda;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken) => agenda.Start(cancellationToken);
+
+    public Task StopAsync(CancellationToken cancellationToken) => agenda.Stop();
+}
