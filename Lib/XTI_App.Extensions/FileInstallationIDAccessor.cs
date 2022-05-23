a0f67c653a66ce7d807e30890634f7b8bcb7410d
jbenfield108@gmail.com <jbenfield108@gmail.com>
2022-05-23 06:02:47 -0400

    Version V1404

---
+++ Lib/XTI_App.Extensions/FileInstallationIDAccessor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using XTI_App.Abstractions;
+using XTI_Core;
+
+namespace XTI_App.Extensions;
+
+internal sealed class FileInstallationIDAccessor : InstallationIDAccessor
+{
+    private readonly IHostEnvironment hostEnv;
+
+    public FileInstallationIDAccessor(IHostEnvironment hostEnv)
+    {
+        this.hostEnv = hostEnv;
+    }
+
+    public async Task<int> Value()
+    {
+        var installationID = 0;
+        var path = Path.Combine(hostEnv.ContentRootPath, "installation.json");
+        if (File.Exists(path))
+        {
+            var content = await File.ReadAllTextAsync(path);
+            var info = XtiSerializer.Deserialize(content, () => new InstallationInfo(0));
+            installationID = info.InstallationID;
+        }
+        return installationID;
+    }
+
+    private sealed record InstallationInfo(int InstallationID);
+}
