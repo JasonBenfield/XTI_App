a50bb06bd53a6403c1f107f00bfaf1dcac63d796
jbenfield108@gmail.com <jbenfield108@gmail.com>
2024-12-26 14:31:47 -0500

    Version V1436

---
+++ Lib/XTI_WebApp.Extensions/XtiMetadataProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace XTI_WebApp.Extensions;
+
+public sealed class XtiMetadataProvider : IMetadataDetailsProvider, IDisplayMetadataProvider
+{
+    public void CreateDisplayMetadata(DisplayMetadataProviderContext context)
+    {
+        if(context.Key.MetadataKind == ModelMetadataKind.Property)
+        {
+            context.DisplayMetadata.ConvertEmptyStringToNull = false;
+        }
+    }
+}
